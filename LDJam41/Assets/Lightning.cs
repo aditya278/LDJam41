d165306e5d90b790872309b9eb9e73973575d603
Chetan <chetan4windows@gmail.com>
2018-04-22 15:52:50 +0530

    Basic lightining

---
+++ LDJam41/Assets/Lightning.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour {
+
+	float minTime=0.5f;
+	float threshold=0.5f;
+	public Light light;
+	float lasttime=0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (Random.Range (1, 30) % 3 == 0) {
+		
+			flash ();
+		}
+		
+	}
+
+	void flash()
+	{
+		if ((Time.time - lasttime) > minTime) {
+
+			if (Random.value > threshold) {
+
+				light.enabled = true;
+			} else {
+
+				light.enabled = false;
+				lasttime = Time.time;
+			}
+		}
+
+	}
+}
