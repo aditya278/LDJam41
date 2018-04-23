f11f479cfd2862eced503d6761f329300b81cc29
Aditya <aditya.shukla278@gmail.com>
2018-04-23 17:51:33 +0530

    Organized

---
+++ LDJam41/Assets/Scripts/BreakMemes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakMemes : MonoBehaviour {
+
+	AudioSource ass;
+	bool onn;
+	// Use this for initialization
+	void Start () {
+		ass = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (!ass.isPlaying && !onn) {
+		
+			StartCoroutine (waitForit ());
+
+		}
+	}
+
+	IEnumerator waitForit(){
+		onn = true;
+		yield return new WaitForSeconds (3f);
+		ass.Play ();
+		onn = false;
+	}
+}
---
+++ LDJam41/Assets/Scripts/footTrail.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class footTrail : MonoBehaviour {
+
+	public GameObject leftFoot, rightFoot;
+	int i=0;
+	public bool calledFootsteps=false,horizontal;
+	public float hrz;
+	public GameObject Player;
+	// Use this for initialization
+	public float h,v;
+	PlayerMovement pm;
+	public AudioSource ass;
+	public AudioClip normal,dirt,toPlay;
+	void Start () {
+
+		toPlay = normal;
+		//Player = gameObject;
+		pm=Player.GetComponent<PlayerMovement>();
+	}
+
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (Input.GetAxis ("Horizontal") == 0 && Input.GetAxis ("Vertical") == 0) {
+		
+
+
+		} else{
+
+
+			h = pm.h;
+			v = pm.v;
+
+			if (h != 0 ) {
+			
+				horizontal = true;
+
+			} else {
+			
+				horizontal = false;
+			}
+
+			if (!calledFootsteps) {
+
+				StartCoroutine (createFootsteps());
+			}
+		}
+	}
+
+	IEnumerator createFootsteps()
+	{
+		calledFootsteps = true;
+		yield return new WaitForSeconds(0.5f);
+
+		float angle = Mathf.Atan2 (v, h) * Mathf.Rad2Deg;
+		if (horizontal) {
+			if (i % 2 == 0) {
+				if (h < 0) {
+				
+					GameObject go = (GameObject)Instantiate (leftFoot, new Vector2 (Player.transform.position.x, Player.transform.position.y - 0.3f), Quaternion.Euler (0f, 0f, -90f + angle ));
+					StartCoroutine (destroyFootsteps (go));
+			
+				} else {
+					
+					GameObject go = (GameObject)Instantiate (rightFoot, new Vector2 (Player.transform.position.x, Player.transform.position.y - 0.3f), Quaternion.Euler (0f, 0f, -90f + angle));
+					StartCoroutine (destroyFootsteps (go));
+				}
+			} else {
+				if (h < 0) {
+					GameObject go = (GameObject)Instantiate (rightFoot, new Vector2 (Player.transform.position.x, Player.transform.position.y + 0.3f), Quaternion.Euler (0f, 0f, -90f + angle));
+					StartCoroutine (destroyFootsteps (go));
+				} else {
+					
+					GameObject go = (GameObject)Instantiate (leftFoot, new Vector2 (Player.transform.position.x, Player.transform.position.y + 0.3f), Quaternion.Euler (0f, 0f, -90f + angle));
+					StartCoroutine (destroyFootsteps (go));
+				}
+			}
+		} else {
+			if (i % 2 == 0) {
+				if (v <= 0) {
+
+					GameObject go = (GameObject)Instantiate (leftFoot, new Vector2 (Player.transform.position.x+0.3f, Player.transform.position.y), Quaternion.Euler (0f, 0f, -90f + angle));
+					StartCoroutine (destroyFootsteps (go));
+
+				} else {
+
+					GameObject go = (GameObject)Instantiate (rightFoot, new Vector2 (Player.transform.position.x + 0.3f, Player.transform.position.y), Quaternion.Euler (0f, 0f, -90f + angle));
+					StartCoroutine (destroyFootsteps (go));
+				}
+			} else {
+				if (v <= 0) {
+					GameObject go = (GameObject)Instantiate (rightFoot, new Vector2 (Player.transform.position.x - 0.3f, Player.transform.position.y), Quaternion.Euler (0f, 0f, -90f + angle));
+					StartCoroutine (destroyFootsteps (go));
+				} else {
+
+					GameObject go = (GameObject)Instantiate (leftFoot, new Vector2 (Player.transform.position.x - 0.3f, Player.transform.position.y), Quaternion.Euler (0f, 0f, -90f + angle));
+					StartCoroutine (destroyFootsteps (go));
+				}
+			}
+		}
+
+		ass.clip = toPlay;
+		ass.Play ();
+		i++;
+		calledFootsteps = false;
+	}
+
+	IEnumerator destroyFootsteps(GameObject gam)
+	{
+		yield return new WaitForSeconds(1.5f);
+		Destroy (gam);
+	}
+
+
+
+}
