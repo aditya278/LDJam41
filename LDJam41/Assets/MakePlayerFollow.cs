08b7ddb2f1e8dc7def77f311d43d206298502763
Aditya <aditya.shukla278@gmail.com>
2018-04-23 17:37:54 +0530

    Added the Target Audio Source.

---
+++ LDJam41/Assets/MakePlayerFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePlayerFollow : MonoBehaviour {
+
+    public Transform playerTransform;
+    public float maxDistance = 50f;
+    public float timeDiff = 60f;
+	// Use this for initialization
+	void Start () {
+		
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timeDiff -= Time.deltaTime;
+        if(timeDiff > 0)
+        {
+            if (Vector3.Distance(transform.position, playerTransform.position) < maxDistance)
+            {
+                Vector3 direction = (playerTransform.position - transform.position).normalized;
+                transform.position -= direction;
+            }
+        }
+
+
+    }
+}
