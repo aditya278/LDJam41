510d2b078c820718c13b718495f6e369fd68a4ba
Chetan <chetan4windows@gmail.com>
2018-04-24 00:51:44 +0530

    fifi

--- LDJam41/Assets/Scripts/PlayerMovement.cs
+++ LDJam41/Assets/Scripts/PlayerMovement.cs
@@ -127,6 +127,6 @@ public class PlayerMovement : MonoBehaviour {
 
 
         angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		//transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 }
